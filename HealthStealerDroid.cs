9bc6caa15c87cff6dc1b43f36e72d1cdc720553f
OleksandrChyzh <147910360+OleksandrChyzh@users.noreply.github.com>
2024-12-20 15:47:13 +0200

    Commit_2

--- HealthStealerDroid.cs
+++ HealthStealerDroid.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace DroidBatlle
 {
-    internal class HealthStealerDroid
+    public class HealthStealerDroid : AttackerDroid
     {
+        public HealthStealerDroid(string name, int health, int attackPower)
+            : base(name)
+        {
+        }
+
+        public override void Attack(BaseDroid enemy)
+        {
+            if (enemy == null || enemy.Health <= 0)
+            {
+                Console.WriteLine($"{Name} не може атакувати!");
+                return;
+            }
+
+            int stolenHealth = Math.Min(AttackPower, enemy.Health);
+            enemy.Health -= stolenHealth;
+            Health += stolenHealth;
+
+            Console.WriteLine($"{Name} краде {stolenHealth} здоров'я у {enemy.Name}. Його здоров'я тепер: {Health}, а {enemy.Name} має: {enemy.Health}.");
+        }
     }
+
 }
